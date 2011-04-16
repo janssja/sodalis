4a33907aa3ba4e1920a3bdd53883cd2256037535
Jackie Janssen <jackiejanssen@me.com>
2011-04-16 21:29:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sodalis::Application.config.secret_token = '3a5b7ede0474625ead0757d974c4e9fb85f044a5e2de9ee4afe244e155a8036d7bc2334b3c8d53e8bdf6557f7f57283ed6d9e5cf995dd67ede3725f25e715c9a'
