26bb5df9b6bef83c1f923540097158cfa42f777b
Jackie Janssen <jackiejanssen@me.com>
2011-04-23 23:52:15 +0200

    added a user controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Sodalis::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
   
   get "users/new"
   
