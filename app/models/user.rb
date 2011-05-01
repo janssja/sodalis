4a5e7d049c0dc580699a6108f9b5d109f955d7f5
Jackie Janssen <jackiejanssen@me.com>
2011-05-01 09:03:37 +0200

    Added successful signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110425182350
+# Schema version: 20110425193730
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
