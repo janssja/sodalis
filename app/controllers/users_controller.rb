6d7c8f09affa860e18ca946726e9caf4966abe9b
Jackie Janssen <jackiejanssen@me.com>
2011-05-02 21:19:43 +0200

    Added some files to Update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,18 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash => { :success => "Sodalis just updated your profile"}
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
 end
